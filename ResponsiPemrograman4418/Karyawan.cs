de9763ae332abc19b7a4503b915302304bd4f98a
fafan ramadhan <forweare7@gmail.com>
2022-04-20 09:54:08 +0700

    Add project files.

---
+++ ResponsiPemrograman4418/Karyawan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4418
+{
+    class Karyawan
+    {
+        //property karyawan
+
+        public string Nik { get; set; }
+        public string Name { get; set; }
+        public float Gajibulanan { get;  set;}
+        
+        //constructor
+        public Karyawan(string nik, string name, float gajibulanan)
+        {
+            Nik = nik;
+            Name = name;
+            Gajibulanan = gajibulanan;
+        }
+
+        //Tambah gaji 10%
+        public void NaikGaji()
+        {
+            Gajibulanan = Gajibulanan + (Gajibulanan * 0.10f);
+        }
+    }
+}
---
+++ ResponsiPemrograman4418/Program.cs
@@ -0,0 +1,23 @@
+﻿//object Karyawan
+
+using ResponsiPemrograman4418;
+
+Karyawan karyawan1 = new Karyawan("12345678", "Joko", 10000000);
+Karyawan karyawan2 = new Karyawan("87654321", "Udin", 20000000);
+
+Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+Console.WriteLine("---------------------------------");
+Console.WriteLine("1    {0}/{1}         {2}", karyawan1.Nik, karyawan1.Name, karyawan1.Gajibulanan);
+Console.WriteLine("2    {0}/{1}         {2}", karyawan2.Nik, karyawan2.Name, karyawan2.Gajibulanan);
+
+Console.WriteLine();
+Console.WriteLine("Naik gaji cuyyy 10%");
+Console.WriteLine();
+
+karyawan1.NaikGaji();
+karyawan2.NaikGaji();
+
+Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+Console.WriteLine("---------------------------------");
+Console.WriteLine("1    {0}/{1}         {2}", karyawan1.Nik, karyawan1.Name, karyawan1.Gajibulanan);
+Console.WriteLine("2    {0}/{1}         {2}", karyawan2.Nik, karyawan2.Name, karyawan2.Gajibulanan);
